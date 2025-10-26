d7bafada174ea845a048156d289c866cdf8e9da8
Steve Yang <stevey@dhcp-10-8-231-139.mobile.reshsg.uci.edu>
2025-10-25 17:18:28 -0700

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', // 👈 important for custom domain
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
